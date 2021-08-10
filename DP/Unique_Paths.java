fa4993ca236239fe66e1934db347145e030dc90b
markliyx <markliyx@sas.upenn.edu>
2021-08-10 18:40:04 -0400

    adding more dp questions

---
+++ DP/Unique_Paths.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        HashMap<Set<Integer>, Integer> memo = new HashMap<Set<Integer>, Integer>();
+        return uniquePathsRecursion(m, n, memo);
+    }
+    
+    public int uniquePathsRecursion(int m, int n, HashMap<Set<Integer>, Integer> memo) {        
+        HashSet<Integer> key = new HashSet<Integer>(Arrays.asList(m, n));
+        if (m == 0 || n == 0) return 0;
+        else if (m == 1 && n == 1) return 1;
+        else if (memo.containsKey(key)) {
+            return memo.get(key);
+        }
+        
+        int memory = uniquePathsRecursion(m - 1, n, memo) + uniquePathsRecursion(m, n - 1, memo);
+        memo.put(key, memory);
+        return memory;
+        
+    }
+}
+
+/*
+62. Unique Paths
+Medium
+
+5987
+
+253
+
+Add to List
+
+Share
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation:
+From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+Example 3:
+
+Input: m = 7, n = 3
+Output: 28
+Example 4:
+
+Input: m = 3, n = 3
+Output: 6
+ 
+
+Constraints:
+
+1 <= m, n <= 100
+It's guaranteed that the answer will be less than or equal to 2 * 109.
+*/
