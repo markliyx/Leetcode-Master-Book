59e8614a793539c3e16df0a3bcc1e468f04db434
markliyx <markliyx@sas.upenn.edu>
2021-09-02 01:04:37 -0400

    new dfs question

---
+++ DFS/Path_Sum_III.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) return 0;
+        return dfs(root, targetSum, 0) + pathSum(root.left, targetSum) + pathSum(root.right, targetSum);
+    }
+    
+    public int dfs(TreeNode curr, int sum, int cum) {
+        if (curr == null) return 0;
+        int right = dfs(curr.right, sum, cum + curr.val);
+        int left = dfs(curr.left, sum, cum + curr.val);
+        if (cum + curr.val == sum) {
+            return left + right + 1;
+        } 
+        return left + right;
+    }
+}
+
+/*
+437. Path Sum III
+Medium
+
+Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+ 
+
+Example 1:
+
+
+Input: root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+Output: 3
+Explanation: The paths that sum to 8 are shown.
+Example 2:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: 3
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 1000].
+-109 <= Node.val <= 109
+-1000 <= targetSum <= 1000
+*/
